0e7acf414026442af2fbb1d2fee55d545cea316f
Hansol-Lim <dev.hansollim@gmail.com>
2018-08-15 19:28:21 +0900

    Add ssh remote url on deploy

--- gh-deploy.sh
+++ gh-deploy.sh
@@ -4,4 +4,5 @@ then
   echo "Which folder do you want to deploy to GitHub Pages?"
   exit 1
 fi
+git remote set-url origin git@github.com:hsol/hsol.github.io.git
 git subtree push --prefix $1 origin gh-pages
