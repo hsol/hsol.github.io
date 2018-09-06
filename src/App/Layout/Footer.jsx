15efe750326e620e6cea8b5694a03a3c51204d67
Hansol-Lim <dev.hansollim@gmail.com>
2018-09-07 04:29:58 +0900

    PC 에서 카카오톡은 팝업으로 열리도록

--- src/App/Layout/Footer.jsx
+++ src/App/Layout/Footer.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 
 class Footer extends React.Component {
+   openKakaoPopup(event) {
+      event.preventDefault();
+
+      window.open('https://open.kakao.com/o/sD6Ru3j', 'Kakotalk to HansolLim', 'width=600,height=800');
+      return false;
+   }
+
    render() {
       return (
          <footer className="card has-background-white">
             <div className="card-footer">
                <a className="card-footer-item" href="https://www.linkedin.com/in/devhansollim/" target="_blank">Linkedin<span
                   className="is-hidden-mobile"> 에서 만나기</span></a>
-               <a className="card-footer-item" href="https://open.kakao.com/o/sD6Ru3j" target="_blank">Kakaotalk<span
+               <a className="card-footer-item" href="https://open.kakao.com/o/sD6Ru3j" target="popup"
+                  onClick={this.openKakaoPopup}>Kakaotalk<span
                   className="is-hidden-mobile">으로 연락하기</span></a>
                <a className="card-footer-item" href="https://facebook.com/molmoty" target="_blank">Facebook <span
                   className="is-hidden-mobile">에서 만나기</span></a>
