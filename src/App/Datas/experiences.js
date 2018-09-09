3277753cff80eb5c43140de86f5238cf0caeceb9
Hansol-Lim <dev.hansollim@gmail.com>
2018-09-10 05:44:44 +0900

    경험 섹션 React 포팅

---
+++ src/App/Datas/experiences.js
@@ -0,0 +1,25 @@
+import { languageConst } from "../Library/Global"
+
+export default {
+   [languageConst.KR]: [
+      { datetime: '2012-03-17', description: '구글 학생 개발자 해커톤 2012' },
+      { datetime: '2014-06-29', description: '2014 빅데이톤 경진대회' },
+      { datetime: '2014-08-01', description: '씨엔티테크(주) 입사' },
+      { datetime: '2014-08-24', description: '구글 개발자 그룹 해커톤(GDG Hackerthon)' },
+      { datetime: '2015-11-30', description: '페이스북 개발자 그룹의 첫 번째 미트업' },
+      { datetime: '2015-07-25', description: 'ERD를 이용한 DB 모델링 세미나' },
+      { datetime: '2015-12-16', description: '테헤란로 커피클럽(아침에 하는 스타트업 네트워킹 모임)_36th' },
+      { datetime: '2016-04-26', description: 'NDC 2016' },
+      { datetime: '2016-07-31', description: '제 3회 UNITHON' },
+      { datetime: '2016-08-27', description: '넥스터즈 9기 활동 수료' },
+      { datetime: '2016-08-31', description: '넥스터즈 10기 운영진(CTO)' },
+      { datetime: '2016-10-25', description: 'DEVIEW 2016' },
+      { datetime: '2016-10-31', description: '씨엔티테크(주) 퇴사' },
+      { datetime: '2016-11-01', description: '리디(주) 입사' },
+      { datetime: '2016-11-05', description: 'GDG Devfest Seoul 2016' },
+      { datetime: '2016-11-26', description: 'XEcon 2016' },
+      { datetime: '2017-02-25', description: '넥스터즈 10기 활동' },
+      { datetime: '2017-04-27', description: 'NDC 2017' },
+      { datetime: '2017-10-27', description: 'DEVIEW 2017' },
+   ]
+}
