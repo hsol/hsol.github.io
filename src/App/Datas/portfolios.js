98ad6b0e4ce28bf912dfc7a57c14dac1901216f8
Hansol-Lim <dev.hansollim@gmail.com>
2018-09-15 08:37:29 +0900

    포트폴리오 init

---
+++ src/App/Datas/portfolios.js
@@ -0,0 +1,11 @@
+import { languageConst } from "../Library/Global"
+
+export default {
+   [languageConst.KR]: [
+      {
+         title: '내 친구 밍기뉴 (Mingginyua)',
+         subtitle: 'IoT 스마트 화분',
+         href: 'https://github.com/Nexters/mingginyu'
+      }
+   ]
+}
