b84c0bfd32222fa2c3358565d73e18318b356f29
hansollim <hansollim@ridi.com>
2018-09-26 12:08:54 +0900

    Add portfolio foreach

--- src/App/Components/Sections/PortfolioSection.jsx
+++ src/App/Components/Sections/PortfolioSection.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import Section from 'Components/Base/Section'
+import Global from 'Library/Global'
 
-import { bulmaCarousel } from 'bulma-extensions'
+import {bulmaCarousel} from 'bulma-extensions'
 
 export default class PortfolioSection extends React.Component {
    constructor(props) {
       super(props);
+
+      this.state = {
+         items: []
+      }
    }
 
-   componentDidMount () {
+   componentDidMount() {
+      this.fetchItems();
       bulmaCarousel.attach();
+   }
+
+   fetchItems() {
+      this.setState({items: Global.getData('portfolios')})
    }
 
    render() {
@@ -28,170 +38,40 @@ export default class PortfolioSection extends React.Component {
                   </div>
                </div>
                <div className="carousel-container columns">
-                  <div className="column carousel-item">
-                     <div className="card">
-                        <header className="card-header has-background-white toggle">
-                           <div className="media card-header-title">
-                              <div className="media-left">
-                                 <figure className="image is-64x64">
-                                    <img src="./images/portfolios/mingginyu.jpg"/>
-                                 </figure>
-                              </div>
-                              <div className="media-content ">
-                                 <p className="is-size-6 has-text-weight-bold">내 친구 밍기뉴 (Mingginyua)</p>
-                                 <p className="subtitle is-size-7">IoT 스마트 화분</p>
-                              </div>
-                           </div>
-                        </header>
-                        <article className="card-content">
-                           <div className="content">
-                              <p><label data-global="period">개발기간</label>. 2<span data-global="day">일</span></p>
-                              <p><label data-global="role">역할</label>. <span data-global="frontend">프론트엔드</span>, <span
-                                 data-global="backend">백엔드</span></p>
-                              <p><label data-global="link">바로가기</label>. <a href="https://github.com/Nexters/mingginyu"
-                                                                            target="_blank">https://github.com/Nexters/mingginyu</a>
-                              </p>
-                           </div>
-                        </article>
+                  {this.state.items.map((item, index) => {
+                     return <div className="column carousel-item">
+                        <div key={index} className='card'>{item.title}</div>
                      </div>
-                  </div>
-                  <div className="column carousel-item">
-                     <div className="card">
-                        <header className="card-header has-background-white toggle">
-                           <div className="media card-header-title">
-                              <div className="media-left">
-                                 <figure className="image is-64x64">
-                                    <img src="./images/portfolios/pizzaalvolo.jpg"/>
-                                 </figure>
-                              </div>
-                              <div className="media-content">
-                                 <p className="is-size-6 has-text-weight-bold">피자알볼로</p>
-                                 <p className="subtitle is-size-7">홈페이지 및 주문시스템</p>
-                              </div>
-                           </div>
-                        </header>
-                        <article className="card-content">
-                           <div className="content">
-                              <p><label data-global="period">개발기간</label>. 3<span data-global="month">개월</span></p>
-                              <p><label data-global="role">역할</label>. <span data-global="frontend">프론트엔드</span>, <span
-                                 data-global="backend">백엔드</span></p>
-                              <p><label data-global="link">바로가기</label>. <a href="https://pizzaalvolo.co.kr"
-                                                                            target="_blank">https://pizzaalvolo.co.kr</a>
-                              </p>
-                           </div>
-                        </article>
-                     </div>
-                  </div>
-                  <div className="column carousel-item">
-                     <div className="card">
-                        <header className="card-header has-background-white toggle">
-                           <div className="media card-header-title">
-                              <div className="media-left">
-                                 <figure className="image is-64x64">
-                                    <img src="./images/portfolios/pizzamaru.jpg"/>
-                                 </figure>
-                              </div>
-                              <div className="media-content">
-                                 <p className="is-size-6 has-text-weight-bold">피자마루</p>
-                                 <p className="subtitle is-size-7">홈페이지</p>
-                              </div>
-                           </div>
-                        </header>
-                        <article className="card-content">
-                           <div className="content">
-                              <p><label data-global="period">개발기간</label>. 2<span data-global="month">개월</span></p>
-                              <p><label data-global="role">역할</label><span data-global="frontend">프론트엔드</span>, <span
-                                 data-global="backend">백엔드</span></p>
-                              <p><label data-global="link">바로가기</label>. <a href="http://pizzamaru.co.kr"
-                                                                            target="_blank">http://pizzamaru.co.kr/</a>
-                              </p>
-                           </div>
-                        </article>
-                     </div>
-                  </div>
-                  <div className="column carousel-item">
-                     <div className="card">
-                        <header className="card-header has-background-white toggle">
-                           <div className="media card-header-title">
-                              <div className="media-left">
-                                 <figure className="image is-64x64">
-                                    <img src="./images/portfolios/coffeezarii.jpg"/>
-                                 </figure>
-                              </div>
-                              <div className="media-content">
-                                 <p className="is-size-6 has-text-weight-bold">커피자리</p>
-                                 <p className="subtitle is-size-7">홈페이지</p>
-                              </div>
-                           </div>
-                        </header>
-                        <article className="card-content">
-                           <div className="content">
-                              <p><label data-global="period">개발기간</label>. 1<span data-global="month">개월</span></p>
-                              <p><label data-global="role">역할</label>. <span data-global="frontend">프론트엔드</span>, <span
-                                 data-global="backend">백엔드</span></p>
-                              <p><label data-global="link">바로가기</label>. <a href="http://coffeezarii.com/"
-                                                                            target="_blank">http://coffeezarii.com/</a>
-                              </p>
-                           </div>
-                        </article>
-                     </div>
-                  </div>
-                  <div className="column carousel-item">
-                     <div className="card">
-                        <header className="card-header has-background-white toggle">
-                           <div className="media card-header-title">
-                              <div className="media-left">
-                                 <figure className="image is-64x64">
-                                    <img src="./images/portfolios/mosburger.jpg"/>
-                                 </figure>
-                              </div>
-                              <div className="media-content">
-                                 <p className="is-size-6 has-text-weight-bold">모스버거</p>
-                                 <p className="subtitle is-size-7">주문 시스템</p>
-                              </div>
-                           </div>
-                        </header>
-                        <article className="card-content">
-                           <div className="content">
-                              <p><label data-global="period">개발기간</label>. 3<span data-global="month">개월</span></p>
-                              <p><label data-global="role">역할</label>. <span data-global="frontend">프론트엔드</span>, <span
-                                 data-global="backend">프론트엔드</span></p>
-                              <p><label data-global="link">바로가기</label>. <a href="http://delivery.moskorea.kr/"
-                                                                            target="_blank">http://delivery.moskorea.kr/</a>
-                              </p>
-                           </div>
-                        </article>
-                     </div>
-                  </div>
-                  <div className="column carousel-item">
-                     <div className="card">
-                        <header className="card-header has-background-white toggle">
-                           <div className="media card-header-title">
-                              <div className="media-left">
-                                 <figure className="image is-64x64">
-                                    <img src="./images/portfolios/thecontest.jpg"/>
-                                 </figure>
-                              </div>
-                              <div className="media-content">
-                                 <p className="is-size-6 has-text-weight-bold">더 콘테스트</p>
-                                 <p className="subtitle is-size-7">Android, iOS 어플리케이션</p>
-                              </div>
-                           </div>
-                        </header>
-                        <article className="card-content">
-                           <div className="content">
-                              <p><label data-global="period">개발기간</label>. 6<span data-global="month">개월</span></p>
-                              <p><label data-global="role">역할</label>. <span data-global="android">안드로이드</span>, <span
-                                 data-global="ios">아이폰</span></p>
-                              <p><label data-global="link">바로가기</label>. <a href="http://theconte.st/androidapp-f"
-                                                                            target="_blank"
-                                                                            data-global="android">안드로이드</a>, <a
-                                 href="http://theconte.st/iosapp-thecontest-f" target="_blank"
-                                 data-global="ios">아이폰</a></p>
-                           </div>
-                        </article>
-                     </div>
-                  </div>
+                  })}
+                  {/*<div className="column carousel-item">*/}
+                  {/*<div className="card">*/}
+                  {/*<header className="card-header has-background-white toggle">*/}
+                  {/*<div className="media card-header-title">*/}
+                  {/*<div className="media-left">*/}
+                  {/*<figure className="image is-64x64">*/}
+                  {/*<img src="./images/portfolios/thecontest.jpg"/>*/}
+                  {/*</figure>*/}
+                  {/*</div>*/}
+                  {/*<div className="media-content">*/}
+                  {/*<p className="is-size-6 has-text-weight-bold">더 콘테스트</p>*/}
+                  {/*<p className="subtitle is-size-7">Android, iOS 어플리케이션</p>*/}
+                  {/*</div>*/}
+                  {/*</div>*/}
+                  {/*</header>*/}
+                  {/*<article className="card-content">*/}
+                  {/*<div className="content">*/}
+                  {/*<p><label data-global="period">개발기간</label>. 6<span data-global="month">개월</span></p>*/}
+                  {/*<p><label data-global="role">역할</label>. <span data-global="android">안드로이드</span>, <span*/}
+                  {/*data-global="ios">아이폰</span></p>*/}
+                  {/*<p><label data-global="link">바로가기</label>. <a href="http://theconte.st/androidapp-f"*/}
+                  {/*target="_blank"*/}
+                  {/*data-global="android">안드로이드</a>, <a*/}
+                  {/*href="http://theconte.st/iosapp-thecontest-f" target="_blank"*/}
+                  {/*data-global="ios">아이폰</a></p>*/}
+                  {/*</div>*/}
+                  {/*</article>*/}
+                  {/*</div>*/}
+                  {/*</div>*/}
                </div>
             </article>
          </Section>
--- src/App/Datas/portfolios.js
+++ src/App/Datas/portfolios.js
@@ -1,11 +1,78 @@
-import { languageConst } from "../Library/Global"
+import {languageConst} from "../Library/Global"
+import moment from 'moment'
+
+export class Portfolio {
+  constructor() {
+    this.title = '';
+    this.subtitle = '';
+    this.link = '';
+
+    this.startDate = null;
+    this.endDate = null;
+
+    this.period = '';
+
+    this.images = [];
+  }
+
+  static create(title) {
+    return (new Portfolio).setTitle(title);
+  }
+
+  setStartDate (startDate) {
+    this.startDate = moment(startDate);
+  }
+
+  setEndDate (endDate) {
+    this.endDate = moment(endDate);
+  }
+
+  setPeriod(startDate = null, endDate = null) {
+    if (startDate !== null) {
+      this.setStartDate(startDate)
+    }
+
+    if (endDate !== null) {
+      this.setEndDate(endDate)
+    }
+
+    if (this.startDate !== null && this.endDate !== null) {
+      this.period = this.startDate.from(this.endDate, true);
+    }
+
+    return this;
+  }
+
+  setTitle(title) {
+    this.title = title;
+
+    return this;
+  }
+
+  setSubtitle(subtitle) {
+    this.subtitle = subtitle;
+
+    return this;
+  }
+
+  setLink(link) {
+    this.link = link;
+
+    return this;
+  }
+
+  addImage(image) {
+    this.images.push(image);
+
+    return this;
+  }
+}
 
 export default {
-   [languageConst.KR]: [
-      {
-         title: '내 친구 밍기뉴 (Mingginyua)',
-         subtitle: 'IoT 스마트 화분',
-         href: 'https://github.com/Nexters/mingginyu'
-      }
-   ]
+  [languageConst.KR]: [
+    Portfolio.create('내 친구 밍기뉴 (Mingginyua)')
+      .setPeriod('2016')
+      .setSubtitle('IoT 스마트 화분')
+      .setLink('https://github.com/Nexters/mingginyu')
+  ]
 }
