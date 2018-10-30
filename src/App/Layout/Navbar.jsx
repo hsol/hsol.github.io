13e0716f1d08ae2e421e4244a1b06301c2ebdb6b
Hansol-Lim <dev.hansollim@gmail.com>
2018-10-30 22:32:19 +0900

    Remove unused navbar

--- src/App/Layout/Navbar.jsx
+++ src/App/Layout/Navbar.jsx
@@ -15,12 +15,6 @@ export default class Navbar extends React.Component {
                      <span className="has-margin-right-10 is-hidden-mobile"/>
                      <span className="level-item is-size-4 is-hidden-mobile has-text-white">임한솔</span>
                   </a>
-                  <a role="button" className="navbar-burger z-index-1 has-text-white" aria-label="menu"
-                     aria-expanded="false">
-                     <span aria-hidden="true"/>
-                     <span aria-hidden="true"/>
-                     <span aria-hidden="true"/>`
-                  </a>
                </div>
             </nav>
          </section>
