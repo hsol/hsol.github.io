5662a13fe2193e2c7e6b896ff74b1837f792841f
Hansol Lim <dev.hansollim@gmail.com>
2022-09-12 08:51:29 +0900

    Use good case

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,37 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
   extends: [
+    'airbnb',
     'plugin:react/recommended',
-    'standard-with-typescript'
-  ],
-  overrides: [
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  plugins: [
-    'react'
+    'plugin:jsx-a11y/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
   ],
   rules: {
-  }
-}
+    'linebreak-style': 0,
+    'import/prefer-default-export': 0,
+    'import/extensions': 0,
+    'no-use-before-define': 0,
+    'import/no-unresolved': 0,
+    'react/react-in-jsx-scope': 0,
+    'import/no-extraneous-dependencies': 0,
+    'no-shadow': 0,
+    'react/prop-types': 0,
+    'react/jsx-filename-extension': [
+      2,
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    ],
+    'jsx-a11y/no-noninteractive-element-interactions': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+};
